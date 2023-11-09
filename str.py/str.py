115074599486221c90a73d385e7e4b8602c96aab
vibedawg <lolkekakniet@mail.ru>
2023-11-09 15:31:40 +0600

    Strings  hw

---
+++ str.py/str.py
@@ -0,0 +1,72 @@
+# firstname="akniet"
+# lastname="rakhymbai"
+# speace=" "
+# full_name=firstname + speace + lastname
+# print(len(firstname)>len(lastname))
+
+
+# abai_poem='''Black of my eye 
+# Frame of my mind 
+# Drink never dry 
+# Love of my Life 
+
+
+# Parting is winter, your absence is sorrow 
+# I wish to stay until the morrow 
+# Between your fair arms 
+# when the nightingale sings, 'Terrow' 
+# '''
+# print(abai_poem)
+
+
+# abai_poem='\nBlack of my eye \nFrame of my mind \nDrink never dry \nLove of my Life ' 
+# print(abai_poem)
+
+# print("Day 1\t5\t5")
+
+# print("Name \tage \tlastname")
+# print("Akniet \t16 \tRakhymbai")
+# print("Symbat \t15 \tMarlenqyzy")
+# print("Yasem \t16 \tChildr")
+
+
+# fname="akniet"
+# lname="rakhymbai"
+# age="16"
+# full_text="I am is {} {}.I am {} yours old".format(fname,lname,age)
+# print(full_text)
+
+# a=2
+# b=4
+# print("{}*{}={}".format(a,b,a*b))
+# print("{}/{}={}".format(a,b,a/b))
+# print("{}+{}={}".format(a,b,a+b))
+# print("{}-{}={}".format(a,b,a-b))
+
+
+# a=2
+# b=4
+# print(f"{a}+{b}={a+b}")
+# print(f"{a}-{b}={a-b}")
+# print(f"{a}/{b}={a/b}")
+# print(f"{a}*{b}={a*b}")
+
+# fname="akniet"
+# lname="rakhymbai"
+# age="16"
+# full_text=(f"I am is {fname} {lname}.I am {age} yours old")
+# print(full_text)
+
+# text="äkjdsagkhgfajhvkahvfkadgfkgvfhgfdkjgjdvmsgfdf"
+# text1=text[0]
+# text2=len(text)-1
+# text3=text[text2]
+# print(text1)
+# print(text3)
+
+
+# contry="Argentina"
+# print(contry[1:8:2])
+
+
+

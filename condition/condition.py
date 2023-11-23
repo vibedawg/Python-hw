572f773d1705f4c102eca130394635d906056861
vibedawg <lolkekakniet@mail.ru>
2023-11-23 15:23:37 +0600

    23.11 hw python

---
+++ condition/condition.py
@@ -0,0 +1,116 @@
+#1
+# age = int(input("Enter your age: "))
+# year = 18
+# if age >= year:
+#     print("You are old enough to learn to drive")
+# elif age <= 0:
+#     print("Error")    
+# else:
+#     wait = year - age 
+#     print(f"You need {wait} more years to learn to drive")
+
+
+
+# age = int(input('Enter your age: '))
+# year = 18 
+# if age < year: 
+#     if age < 0:
+#         print('Error')
+#     else:
+#         print(f'You need {year - age} more years to learn to drive.')
+# else:
+#     print('You are old enough to drive')   
+
+
+
+
+#2
+# your_age = int(input("Enter your age: ")) 
+# my_age = 16
+# if your_age >= my_age:
+#     if your_age < 0: 
+#         print("Error")
+#     else:    
+#         print(f"You are {your_age - my_age} years older than me") 
+# elif my_age >= your_age:
+#         print(F"You are {my_age - your_age} years younger than me")
+
+
+#3  
+# num_1 = int(input("Enter number one: "))
+# num_2 = int(input("Enter number two: "))  
+# if num_1 >= num_2:
+#      if num_1 <= num_2:
+#           print(f"{num_2} is greater than {num_1}") 
+#      else: 
+#           print(f"{num_1} is greater than {num_2}") 
+
+
+#4
+# a = int(input("Enter your grade: "))    
+# if a >= 80 and a <= 100:
+#      print("Your grade is A")  
+# elif a >= 70 and a <= 89:
+#           print("Your grade is B") 
+# elif a >= 60 and a <= 69:
+#           print("Your grade is c")
+# elif a >= 50 and a <= 59:
+#           print("Your grade is D") 
+# elif a >= 0 and a <= 49:
+#           print("Your grade is F")
+# else:
+#         a <= 0 
+#         print("ERROR")           
+
+
+#5 
+# user = input("Enter month: ")
+# season = ""
+# if user in ["September","October","November"]:
+#         season="Autumn"
+# elif user in ["December","January","February"]:
+#         season="Winter"
+# elif user in ["March","April","May"]:
+#        season="Spring"                
+# elif user in ["June","July","August"]:
+#         season="Summer"
+# print(f"The season for {user} is {season}")  
+
+
+#6
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruit = input('Enter friut: ')
+# if fruit in fruits:
+#     print('That fruit already exist in the list')
+# else:
+#     fruits.append(fruit)
+#     print(f'We have added the fruit to the list:{fruits}')
+
+
+#lvl 3
+# person={
+# 'first_name': 'Asabeneh',
+# 'last_name': 'Yetayeh',
+# 'age': 250,
+# 'country': 'Finland',
+# 'is_marred': True,
+# 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+# 'address': {
+# 'street': 'Space street',
+# 'zipcode': '02210'
+# }
+# }
+
+#1
+# if 'skills' in person:
+#     print(person['skills'][len(person['skills'])//2])
+# else:
+#     print("Error")
+
+#2
+# if 'skills' in person:
+#     if 'Python' in person['skills']:
+#      print('This skill is on the dict')
+
+
+#3
